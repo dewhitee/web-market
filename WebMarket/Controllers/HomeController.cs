4ac6440a701681dfdbca95e6388da1ad7806a943
DewhiteE <dewhiteeofficial@gmail.com>
2020-03-07 00:31:08 +0200

    temp

--- WebMarket/Controllers/HomeController.cs
+++ WebMarket/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebMarket.Controllers
             return View();
         }
 
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
