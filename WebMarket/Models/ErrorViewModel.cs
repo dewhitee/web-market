b25407fa3940ed159fd5356fdf675a47a9a695b2
DewhiteE <dewhiteeofficial@gmail.com>
2020-03-06 18:47:24 +0200

    temp

---
+++ WebMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
