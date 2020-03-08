16b492bff79946fb5d35bda4e37d401513dcb826
DewhiteE <dewhiteeofficial@gmail.com>
2020-03-08 14:03:40 +0200

    add CatalogViewModel

---
+++ WebMarket/Models/CatalogViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMarket.Models
+{
+    public class CatalogViewModel
+    {
+        public class Product
+        {
+            public int ProductIndex { get; set; }
+            public string ProductName { get; set; }
+            public string ProductType { get; set; }
+            public int CostIntegral { get; set; }
+            public int CostFractional { get; set; }
+            public float Discount { get; set; }
+            public string Description { get; set; }
+        }
+
+        List<Product> Products { get; set; }
+    }
+}
