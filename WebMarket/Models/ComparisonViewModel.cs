a031ae7dbfac1b33558ed8b879e2fe2cd07416a8
DewhiteE <dewhiteeofficial@gmail.com>
2020-04-08 19:29:42 +0300

    add Comparison view model, controller and view

---
+++ WebMarket/Models/ComparisonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMarket.Models
+{
+    public class ComparisonViewModel
+    {
+        public static List<CatalogViewModel.Product> Products = new List<CatalogViewModel.Product>();
+    }
+}
