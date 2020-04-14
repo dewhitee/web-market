adbffc94afa568b737ca39504406312e655bcb3f
DewhiteE <dewhiteeofficial@gmail.com>
2020-04-14 15:22:55 +0300

    Update Userbase.cs

--- WebMarket/Data/Userbase.cs
+++ WebMarket/Data/Userbase.cs
@@ -8,6 +8,11 @@ namespace WebMarket.Data
 {
     public class Userbase
     {
+        public static void Set(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            SignInManager = signInManager;
+            UserManager = userManager;
+        }
         public static SignInManager<IdentityUser> SignInManager { get; set; }
         public static UserManager<IdentityUser> UserManager { get; set; }
     }
