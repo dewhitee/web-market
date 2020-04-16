2868c61afb3f120e299e1a61801d6ba552e90d71
DewhiteE <dewhiteeofficial@gmail.com>
2020-04-16 17:33:38 +0300

    tmp

--- WebMarket/Models/ComparisonViewModel.cs
+++ WebMarket/Models/ComparisonViewModel.cs
@@ -17,20 +17,20 @@ namespace WebMarket.Models
         public static string PriceComparisonText()
         {
             return (LeftProduct.Price > RightProduct.Price) ? $"{LeftProduct.Name} is more expensive than {RightProduct.Name}"
-                : LeftProduct.Price == RightProduct.Price ? $"{LeftProduct.Name} is equal in terms of price with {RightProduct.Name}"
-                : $"{LeftProduct.Name} is cheaper than {RightProduct.Name}";
+                : LeftProduct.Price < RightProduct.Price ? $"{LeftProduct.Name} is cheaper than {RightProduct.Name}"
+                : $"{LeftProduct.Name} and {RightProduct.Name} are equal in terms of price ";
         }
         public static string DiscountComparisonText()
         {
             return (LeftProduct.Discount > RightProduct.Discount) ? $"{LeftProduct.Name} has larger discount than {RightProduct.Name}"
-                : LeftProduct.Discount == RightProduct.Discount ? $"{LeftProduct.Name} has same discount as {RightProduct.Name}"
-                : $"{LeftProduct.Name} has smaller discount than {RightProduct.Name}";
+                : LeftProduct.Discount < RightProduct.Discount ? $"{LeftProduct.Name} has smaller discount than {RightProduct.Name}"
+                : $"{LeftProduct.Name} has same discount as {RightProduct.Name}";
         }
         public static string AddedDateComparisonText()
         {
             return (LeftProduct.AddedDate > RightProduct.AddedDate) ? $"{LeftProduct.Name} was added later than {RightProduct.Name}"
-                : LeftProduct.AddedDate == RightProduct.AddedDate ? $"{LeftProduct.Name} was added at the same day with {RightProduct.Name}"
-                : $"{LeftProduct.Name} was added earlier than {RightProduct.Name}";
+                : LeftProduct.AddedDate < RightProduct.AddedDate ? $"{LeftProduct.Name} was added earlier than {RightProduct.Name}"
+                : $"{LeftProduct.Name} was added at the same day with {RightProduct.Name}";
         }
         public static string[] GetProductNames()
         {
