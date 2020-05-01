a423ce488db951807f2f92fcd6a85843e0054e33
DewhiteE <dewhiteeofficial@gmail.com>
2020-05-01 16:17:30 +0300

    add ModelBuilderExtensions static class

---
+++ WebMarket/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMarket.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ID = 1,
+                    Name = "TestProduct",
+                    Type = "Software",
+                    Price = 10.0M,
+                    Discount = 30f
+                },
+                new Product
+                {
+                    ID = 2,
+                    Name = "AnotherTestProduct",
+                    Type = "Game",
+                    Price = 14.990M,
+                    Discount = 10f
+                }
+            );
+        }
+    }
+}
