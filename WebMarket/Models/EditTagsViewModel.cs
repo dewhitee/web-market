b525e71225fd3988496287a2b6f71cc6966aaadd
DewhiteE <dewhiteeofficial@gmail.com>
2020-05-14 15:53:55 +0300

    Update EditTagsViewModel.cs

--- WebMarket/Models/EditTagsViewModel.cs
+++ WebMarket/Models/EditTagsViewModel.cs
@@ -7,5 +7,17 @@ namespace WebMarket.Models
 {
     public class EditTagsViewModel
     {
+        public int ProductId { get; set; }
+        // IEnumerable<string> Tags { get; set; }
+
+        //! there will be max 8 tags approx. available to the product in the end
+        public string FirstTag { get; set; }
+        public string SecondTag { get; set; }
+        public string ThirdTag { get; set; }
+        public string FourthTag { get; set; }
+        public string FifthTag { get; set; }
+        public string SixthTag { get; set; }
+        public string SeventhTag { get; set; }
+        public string EighthTag { get; set; }
     }
 }
