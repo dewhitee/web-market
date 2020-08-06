ec1f180fc790eb8539d87e2cc6a8dcb677a8b6b6
DewhiteE <dewhiteeofficial@gmail.com>
2020-08-06 20:59:59 +0300

    add AdminWarning partial view

---
+++ WebMarket/Models/ProductModels/EditDownloadsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMarket.Models.ProductModels
+{
+    public class EditDownloadsViewModel
+    {
+        public int ProductId { get; set; }
+        public IFormFile ZipFile { get; set; }
+        public bool Successful { get; set; }
+    }
+}
